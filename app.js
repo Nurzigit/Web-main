5d1ff15e9e3c567cd8ca2627b41e41f61b11964d
Nurzhigit Turman <nurzhigitturman@MacBook-Air-Nurzhigit.local>
2023-12-20 01:16:23 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+// Чтение данных из файла
+fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
+
+    // Преобразование JSON строки в объект
+    const jsonData = JSON.parse(data);
+
+    // Далее вы можете работать с объектом jsonData
+    runServer(jsonData);
+});
+
+app.use(express.static('public/css'));
+app.use(express.static('public/js'));
+app.use(express.static('public/images'));
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+// app.get('/', (req, res) => {
+//     res.render('index', {data})
+// })
+
+// app.get('/about', (req, res) => {
+//     res.render('src/about', {text: 'Привет это об нас страница'})
+// })
+
+function runServer(data) {
+    app.set('view engine', 'ejs');
+
+    // Рендеринг шаблона index.ejs с передачей данных
+    app.get('/', (req, res) => {
+        res.render('index', { data: data.items });
+    });
+
+    const port = 3001;
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}
